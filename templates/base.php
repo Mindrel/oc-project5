019f1a0bacfe7d356a4b3653ba87d1d8bd038635
Mindrel <juanmi66@gmail.com>
2020-08-30 12:19:54 +0200

    Design header logo et avatar

--- templates/base.php
+++ templates/base.php
@@ -1,18 +1,57 @@
 <!DOCTYPE html>
 
-<!-- Template principal -->
+<!-- Template de base -->
 
 <html lang="fr">
 
 <head>
     <meta charset="utf-8" />
+    <meta name="description" content="Le portfolio d'un Développeur Web Junior perdu dans les méandres de la complexité du code... Le blog est mis à jour régulièrement, alors à très vite !" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <script src="https://kit.fontawesome.com/bb97965415.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&family=Open+Sans&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="../public/css/main.css" />
+    <link rel="stylesheet" href="../public/css/header.css" />
     <title><?= $title ?></title>
 </head>
 
 <body>
-    <div id="content">
-        <?= $content ?>
-    </div>
+    <header>
+        <div class="container header-container">
+            <!-- Logo -->
+            <div class="header-logo">
+                <div class="header-logo-left-brace">
+                    {
+                </div>
+
+                <div class="header-logo-text">
+                    <p><strong>Michel Martin</strong></p>
+                    <p><strong><span class="header-logo-text-spe">Junior</span> Web Developer</strong></p>
+                </div>
+
+                <div class="header-logo-right-brace">
+                    }
+                </div>
+            </div>
+
+            <!-- Main menu -->
+            <nav>
+                <ul>
+                    <li><a href="../public/index.php">Hello :)</a></li>
+                    <li><a href="#">Projets</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Le blog</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section>
+        <img src="../public/img/avatar.svg" alt="avatar" />
+
+    </section>
+
+
 </body>
 
 </html>
