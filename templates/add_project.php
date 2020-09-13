36aac42db9aa7eeab1208117091baee22cd90bfa
Mindrel <juanmi66@gmail.com>
2020-09-13 11:11:10 +0200

    Design ajout article et form

--- templates/add_project.php
+++ templates/add_project.php
@@ -2,11 +2,25 @@
 
 <?php $this->title = "Administration - Nouveau projet" ?>
 
+<section id="admin">
+    <!-- Hero secondaire -->
+    <div id="hero" class="hero-container-secondary">
+        <h1>Administration</h1>
 
-<div>
-    <?php include("form_project.php") ?>
-
-    <div>
-        <a href="index.php?route=administration" class="text-link" onclick="return confirm('Revenir au hub ? \nATTENTION : Toute modification sera perdue.')">Retour au hub admin</a> | <a href="index.php" class="text-link" onclick="return confirm('Revenir à l\'accueil ? \nATTENTION : Toute modification sera perdue.')">Retour à l'accueil</a> | <a href="index.php?route=blog" class="text-link" onclick="return confirm('Revenir au blog ? \nATTENTION : Toute modification sera perdue.')">Retour au blog</a>
+        <div class="white-divider">
+            <div class="white-divider-line"></div>
+            <div class="white-divider-icon"><i class="fas fa-code"></i></div>
+            <div class="white-divider-line"></div>
+        </div>
     </div>
-</div>
+
+    <div class="container admin-section-container">
+        <h3>Ajout d'un projet</h3>
+        
+        <?php include("form_project.php") ?>
+
+        <div>
+            <a href="index.php?route=administration" class="text-link" onclick="return confirm('Revenir au hub ? \nATTENTION : Toute modification sera perdue.')">Retour au hub admin</a> | <a href="index.php" class="text-link" onclick="return confirm('Revenir à l\'accueil ? \nATTENTION : Toute modification sera perdue.')">Retour à l'accueil</a> | <a href="index.php?route=blog" class="text-link" onclick="return confirm('Revenir au blog ? \nATTENTION : Toute modification sera perdue.')">Retour au blog</a>
+        </div>
+    </div>
+</section>
--- templates/form_article.php
+++ templates/form_article.php
@@ -8,13 +8,11 @@ $submit = $route === "addArticle" ? "Envoyer" : "Mettre à jour";
 ?>
 
 <form method="post" action="index.php?route=<?= $route ?>">
-    <label for="title">Titre</label><br />
-    <input type="text" id="title" name="title" value="<?= isset($post) ? htmlspecialchars($post->get("title")): "" ?>" /><br />
-    <?= isset($errors["title"]) ? $errors["title"] : "" ?>
+    <label for="title">Titre</label><?= isset($errors["title"]) ? $errors["title"] : "" ?>
+    <input type="text" id="title" name="title" value="<?= isset($post) ? htmlspecialchars($post->get("title")): "" ?>" />
 
-    <label for="content">Contenu</label><br>
-    <textarea id="content" name="content"><?= isset($post) ? htmlspecialchars($post->get("content")): "" ?></textarea><br />
-    <?= isset($errors["content"]) ? $errors["content"] : "" ?>
+    <div><label for="content">Contenu</label><?= isset($errors["content"]) ? $errors["content"] : "" ?></div>
+    <textarea id="content" name="content"><?= isset($post) ? htmlspecialchars($post->get("content")): "" ?></textarea>
 
-    <input type="submit" value="<?= $submit ?>" id="submit" name="submit" />
+    <input type="submit" value="<?= $submit ?>" id="submit" name="submit" class="colored-submit-button" />
 </form>
