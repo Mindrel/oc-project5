a6ba9dd1c95f83ce4aeae84fad57c74de4eb3b1c
Mindrel <juanmi66@gmail.com>
2020-09-27 00:25:29 +0200

    Modif météo ne bascule plus auto si écran <768px

--- public/js/toggle_weather.js
+++ public/js/toggle_weather.js
@@ -3,8 +3,8 @@ function toggleWeather() {
     document.getElementById("weather").classList.toggle("active"); // Inverse la valeur false ou true
 }
 
-// Déclenche auto toggleWeather() dès que page chargée
-document.addEventListener("DOMContentLoaded", toggleWeather);
-
-// Rebascule auto au bout de 2s
-setTimeout(toggleWeather, 2000);
+// Déclenche auto toggleWeather() dès que page chargée si écran > SM
+if (window.matchMedia("(min-width: 768px)").matches) {
+    document.addEventListener("DOMContentLoaded", toggleWeather);
+    setTimeout(toggleWeather, 2000); // Rebascule auto au bout de 2s
+}
